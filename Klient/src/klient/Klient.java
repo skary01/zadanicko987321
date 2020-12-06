7ff259363546600d10c985d0f5ddd0c1c7dc7453
xondirko <75565287+xondirko@users.noreply.github.com>
2020-12-06 11:58:08 +0100

    Add files via upload
    Klient v.1.1 repair

--- Klient/src/klient/Klient.java
+++ Klient/src/klient/Klient.java
@@ -1,60 +1,37 @@
 package klient;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.InetAddress;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
+
 /**
  * This class implements java socket client
- * @author Ondo
+ * @author Ondirko, Karabinos
  *
  */
 public class Klient {
 
-    public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
-        //get the localhost IP address, if server is running on some other IP, you need to use that
-        InetAddress host = InetAddress.getLocalHost();
-        
-        Socket socket;
-        ObjectOutputStream objectOutputStream ;
-        ObjectInputStream objectInputStream ;
-        
-        for(int i=1; i<6;i++){
-            socket = new Socket(host.getHostName(), 9876);
-            //establish socket connection to server
-            
-            //write to socket using ObjectOutputStream
-            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            if(i==5)objectOutputStream.writeObject("exit");
-            else{
-                if(i%2!=0)
-                {
-                    System.out.println("Zadaj Id");
-                    Scanner sc = new Scanner( System.in );
-                    String id = sc.next();
-                    objectOutputStream.writeObject(""+id);
-                }
-                if(i%2==0)
-                {
-                    System.out.println("Zadaj heslo");
-                    Scanner sc = new Scanner( System.in );
-                    String heslo = sc.next();
-                    objectOutputStream.writeObject(""+heslo);
-                }
-            }
-            //read the server response message
-          objectInputStream = new ObjectInputStream(socket.getInputStream());
-          String message = (String) objectInputStream.readObject();
-            System.out.println("Prijata sprava: " + message);
-            //close resources
-            
-            Thread.sleep(1000);
-            objectInputStream.close();
-            objectOutputStream.close();
+    public static void main(String[] args) throws Exception
+    { 
+        Socket s = new Socket("localhost",3333);
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader stdin = new BufferedReader(isr);
+        String s1="",s2="";
+        System.out.println("'s' pre studenta 'r' pre refeerenta");  
+        while(!s1.equals("stop"))
+        {         
+            s2=stdin.readLine();
+            dos.writeUTF(s2);
+            dos.flush();
+            s1=dis.readUTF();
+            System.out.println("Server: "+s1);
         }
-        
+        dos.close();
+        s.close();
     }
 }
